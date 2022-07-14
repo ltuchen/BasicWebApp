f4743cd01962637efa89196ee814d150905b2288
Lucas Tuchen <lucas.tuchen@tum.de>
2022-07-14 13:02:38 +0200

    ÜBERTREIB MAL NICHT

--- src/main/java/de/tum/in/ase/eist/AnswerResource.java
+++ src/main/java/de/tum/in/ase/eist/AnswerResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AnswerResource {
 
 
+
 	private final Logger logger = LoggerFactory.getLogger(AnswerResource.class);
 
 	private final QueryProcessor queryProcessor;
